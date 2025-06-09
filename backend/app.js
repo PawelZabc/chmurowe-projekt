46955e6c0da91c9df4b59eea6b4c34f2ffedb405
Paweł Żabczyński <p_k_zab@wp.pl>
2025-06-10 01:14:41 +0200

    added sending and reading messages to frontend and backend

--- backend/app.js
+++ backend/app.js
@@ -66,27 +66,72 @@ app.post('/api/signup', async (req, res) => {
     }
   });
 
-// Dodawanie nowej wiadomości POST /api/message z JSON: { userId, content }
-app.post('/api/message', async (req, res) => {
-  const { userId, content } = req.body;
+  
+// app.post('/api/message', async (req, res) => {
+//   const { userId, content } = req.body;
 
-  if (!userId || !content) {
-    return res.status(400).json({ error: 'Missing userId or content' });
+//   if (!userId || !content) {
+//     return res.status(400).json({ error: 'Missing userId or content' });
+//   }
+
+//   try {
+//     const result = await db.query(
+//       'INSERT INTO messages (user_id, content) VALUES ($1, $2) RETURNING id, content, created_at',
+//       [userId, content]
+//     );
+
+//     res.status(201).json(result.rows[0]);
+//   } catch (err) {
+//     console.error('Error adding message:', err);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+
+app.get('/api/message/', async (req,res)=>{
+  const {userFilter,contentFilter} = req.query;
+  let baseQuery = 'SELECT messages.content, messages.created_at, users.username FROM messages JOIN users ON messages.user_id = users.id';
+  const conditions = [];
+  const params = [];
+
+
+  if (userFilter) {
+    params.push(userFilter);
+    conditions.push(`username = $${params.length}`);
   }
 
-  try {
-    const result = await db.query(
-      'INSERT INTO messages (user_id, content) VALUES ($1, $2) RETURNING id, content, created_at',
-      [userId, content]
-    );
-
-    res.status(201).json(result.rows[0]);
-  } catch (err) {
-    console.error('Error adding message:', err);
-    res.status(500).json({ error: 'Internal server error' });
+  if (contentFilter) {
+    params.push(`%${contentFilter}%`);
+    conditions.push(`content ILIKE $${params.length}`);
   }
+
+  if (conditions.length > 0) {
+    baseQuery += ' WHERE ' + conditions.join(' AND ');
+  }
+
+  baseQuery += ' ORDER BY created_at DESC LIMIT 20';
+
+  const result = await db.query(baseQuery, params);
+
+  res.json(result.rows);
+
 });
 
+app.post('/api/message/', async (req,res)=>{
+  const { user_id, content} = req.body;
+  console.log(user_id,content)
+  if (!user_id) {
+    return res.status(401).json({ message: 'You need to be logged in' });
+  }
+  if (!content){
+    return res.status(400).json({ message: 'The message cant be empty' });
+  }
+  const result = await db.query(
+          'INSERT INTO messages (user_id, content) VALUES ($1, $2) RETURNING id',
+          [user_id, content]
+        );
+  res.status(200).json({message:"Message added"});
+
+});
 
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
